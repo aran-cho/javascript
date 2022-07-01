12c24c5a298a10352a080142bab8efffba79d8eb
aran-cho <oing9605@gmail.com>
2022-07-01 12:01:19 +0900

    commits

---
+++ variableStrings.js
@@ -0,0 +1,20 @@
+let numberFirst = 3;
+let stringSecond = "5";
+console.log(numberFirst + stringSecond);        //문자로 변환
+console.log(numberFirst - stringSecond);
+console.log(numberFirst * stringSecond);        //숫자로 인식
+
+stringSecond = "A";
+console.log(numberFirst + stringSecond);
+console.log(numberFirst - stringSecond);
+console.log(numberFirst * stringSecond);
+
+let animal = "tiger";
+console.clear();
+
+// animal.replace("t", "T")
+// 'Tiger'
+// animal.replace("ger","gers")
+// 'tigers'
+// animal.substring(0, 2)
+// 'ti'
