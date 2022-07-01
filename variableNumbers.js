e4b49135acff5a4c318bc443b4273bb9688829f3
aran-cho <oing9605@gmail.com>
2022-07-01 12:01:25 +0900

    commits

---
+++ variableNumbers.js
@@ -0,0 +1,12 @@
+let numberFirst = 3;
+let numberSecond = 5;
+console.log(numberFirst + numberSecond);
+
+// let numberthird = 7;
+// console.log(numberFirst + numberthird);
+//재활용하는 방식으로 하기
+numberSecond = 7;
+console.log(numberFirst + numberSecond);
+
+let numberResult = numberFirst + numberSecond;
+console.log(numberResult);
