d29e52b756b559c645231a8971092399cd999bfc
aran-cho <oing9605@gmail.com>
2022-07-05 22:05:20 +0900

    commit

--- self_numbers.js
+++ self_numbers.js
@@ -1,15 +1,9 @@
+let sum = 0;
+let arr = new Array(10000).fill(true);// array로 해야지 속도가 빨라서 
 
-
-
-function sumThree(item_1,item_2,item_3){
-    let result = item_1 + item_2 + item_3;
-    return result;
+function self(N){
+    [...N.toString].forEach((nums) => {
+        sum += Number(nums)        
+    });
+        
 }
-let first = 2;
-let second = 3;
-let third = 4;
-results = sumThree(first,second,third);
-let backtick = `sumThree() fucntinon : ${results}`;
-console.log(backtick);
-
-
