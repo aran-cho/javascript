52115a9a06a347d915c07dc2378787a34307bc52
aran-cho <oing9605@gmail.com>
2022-07-06 08:49:08 +0900

    commit

--- self_numbers.js
+++ self_numbers.js
@@ -1,9 +1,25 @@
+//재오 코드
 let sum = 0;
-let arr = new Array(10000).fill(true);// array로 해야지 속도가 빨라서 
+let arr = new Array(10000).fill(true); // array로 해야지 속도가 빨라서 
 
 function self(N){
-    [...N.toString].forEach((nums) => {
+    [...N.toString()].forEach((nums) => {
         sum += Number(nums)        
     });
-        
+     
+    sum += Number(i);
+
+    arr[sum] = false;
+
+    sum = 0;
+}
+
+for (i =1; i <= 10000; i++){
+    self(i);
+}
+
+for (i =1; i < 10001; i++){
+    if(arr[i]==true){
+        console.log(i);
+    }
 }
