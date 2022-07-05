125660bbbf1e01ed4569c9470e9f58321f3c05f1
aran-cho <oing9605@gmail.com>
2022-07-05 21:31:13 +0900

    commit

---
+++ self_numbers.js
@@ -0,0 +1,15 @@
+
+
+
+function sumThree(item_1,item_2,item_3){
+    let result = item_1 + item_2 + item_3;
+    return result;
+}
+let first = 2;
+let second = 3;
+let third = 4;
+results = sumThree(first,second,third);
+let backtick = `sumThree() fucntinon : ${results}`;
+console.log(backtick);
+
+
