e61536ab1560a8f42fba375ab52624972065a5c9
aran-cho <oing9605@gmail.com>
2022-07-07 10:11:44 +0900

    dom commit

--- js/doms.js
+++ js/doms.js
@@ -26,13 +26,74 @@ function add(first='0'){
 
     return true;
 
-
-
 }
 
 
+
+
+// arrow function (단순화시키기)->하단에 넣기위해 따로 써놓음
+// (first='0' )=>{
+//     console.log(`add : ${first}`)
+
+//     let ulTag = document.querySelector('ul');
+//     let liTag = document.createElement('li');
+//     let liText = document.createTextNode('Sony')
+//     liTag.appendChild(liText);
+//     ulTag.appendChild(liTag);
+
+//     return true;
+// }
+
 let addEvent = document.querySelector("#add");
-addEvent.addEventListener('click', add);
+// addEvent.addEventListener('click', add);
+// addEvent.addEventListener('click', (first='0' )=>{
+//     console.log(`add : ${first}`)
+
+//     let ulTag = document.querySelector('ul');
+//     let liTag = document.createElement('li');
+//     let liText = document.createTextNode('Sony')
+//     liTag.appendChild(liText);
+//     ulTag.appendChild(liTag);
+
+//     return true;
+// });
+
+
+
+
+addEvent.addEventListener('click', (event)=>{
+    // console.log(event);
+    console.log(event.target.innerText);
+
+    let ulTag = event.target;
+    let liTag = document.createElement('li');
+    let liText = document.createTextNode('Sony')
+    liTag.appendChild(liText);
+    ulTag.appendChild(liTag);
+//버튼 클릭했을때 소니가 바로 빝에 붙음
+    return true;
+});
+
+// =
+// function add(event){
+//     console.log(`add : ${event}`)
+
+//     let ulTag = document.querySelector('ul');
+//     let liTag = document.createElement('li');
+//     let liText = document.createTextNode('Sony')
+//     liTag.appendChild(liText);
+//     ulTag.appendChild(liTag);
+
+//     return true;
+
+// }
+
+
+
+
+
+
+
 
 let removeEvent = document.querySelector('#remove');
 removeEvent.addEventListener('click', remove);
